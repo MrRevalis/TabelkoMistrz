262e98bbf4a95981acbbe4e9796ade426e2a6e01
Daniel Wojdak <logiblogi@gmail.com>
2020-12-30 19:58:38 +0100

    Fix koloru komórki

--- js/exporter.js
+++ js/exporter.js
@@ -44,7 +44,7 @@ Exporter.genLatexCode = function(){
                     result = "\\textcolor[RGB]{"+cell.style.color.replace("rgb(","").replace(")","")+"}{"+result+"}";
                 }
                 //check cell color
-                if(cell.style.backgroundColor != "white"){
+                if(cell.style.backgroundColor != "white" && cell.style.backgroundColor != ""){
                     result = "\\cellcolor[RGB]{"+cell.style.backgroundColor.replace("rgb(","").replace(")","")+"}"+result;
                 }
                 //check multirow
