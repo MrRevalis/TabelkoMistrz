d70d0a2e2140367d958666555d1f1e786a93b8fa
Daniel Wojdak <logiblogi@gmail.com>
2021-01-10 20:22:47 +0100

    Importer LaTeX - krawędzie poziome i pionowe

--- js/importer.js
+++ js/importer.js
@@ -79,6 +79,46 @@ Importer.loadLatex = function(){
     //create table
     CreateTable(rowsCount, cols);
     const table = document.getElementById("mainTable");
+
+    if(!vlines.includes(0)){
+        TableBorderChange(['0', '1']);
+    }
+
+    for(let i = 0; i < table.rows.length; i++){
+        const thisRow = rows[i].replace("\\&", "<,UMPERDAND.>").split("&");
+        const rowSpec = Importer.manageFirstCell(thisRow[0]);
+        thisRow[0] = Importer.clearFirstCell(thisRow[0], rowSpec);
+
+        let tborders = [];
+        //get cols with top borders
+        if(rowSpec[0]){
+            if(rowSpec[0][0] != -1)
+            tborders = Importer.getIdFromCline(rowSpec[0]);
+            else {
+                for(let i=0;i<cols;i++) tborders.push(i);
+            }
+        }
+        
+        //loop throunght cells in row
+        for(let j = 0; j < cols; j++){
+            const cell = document.getElementById(i+":"+j);
+            cell.textContent = thisRow[j].replace("<,UMPERDAND.>", "&");
+            //set top border
+            if(tborders.includes(j)){
+                wybranaKomorka = i+":"+j;
+                AddBorderToCell(['0','1','0','0']);
+                wybranaKomorka = (i-1)+":"+j;
+                AddBorderToCell(['0','0','0','1']);
+                wybranaKomorka = null;
+            }
+        }
+
+        //set row height
+        if(rowSpec[1]){
+            document.getElementById("h:"+(i-1)).textContent = rowSpec[1].replaceAll(" ", "");
+            heightTooBarArray[i-1] = "["+rowSpec[1].replaceAll(" ", "")+"]";
+        }
+    }
 }
 //zwraca caly tekst az do zamkniecia klamerek
 Importer.getToEnd = function(code, idx){
@@ -128,6 +168,119 @@ Importer.manageHeader = function(code){
         }
     }
     //get vlines
+    let pos = 0;
+    for(let i = 0; i < code.length; i++){
+        if(validSpec.includes(code[i])){
+            if(code[i-1] == '|'){
+                vlines.push(1);
+            } else {
+                vlines.push(0);
+            }
+        }
+        if(skipSpec.includes(code[i])){
+            if(code[i-1] == '|'){
+                vlines.push(1);
+            } else {
+                vlines.push(0);
+            }
+            i += Importer.getToEnd(code, i+2).length + 1;
+        }
+    }
+    if(code[code.length-1] == '|'){
+        vlines.push(1);
+    } else {
+        vlines.push(0);
+    }
 
     return [cols,vlines,colsSpec,colsExtras];
+}
+//zwraca komende w podanym kodzie na podanym miejscu oraz argument
+Importer.getCommand = function(code, idx){
+    const knownCommandsNA = ["hline"];
+    const knownCommandsWA = ["cline", "textbf"];
+    let cmd = "";
+    let arg = "";
+    let cmdReady = false;
+
+    for(let i = idx; i < code.length; i++){
+        if(!cmdReady){
+            cmd += code[i];
+            if(knownCommandsNA.includes(cmd)){
+                return [cmd, null];
+            } else if(knownCommandsWA.includes(cmd)){
+                cmdReady = true;
+            }
+        } else {
+            if(code[i] == '{'){
+                const arg = Importer.getToEnd(code, i+1);
+                return [cmd, arg];
+            }
+        }
+    }
+    return [undefined, null];
+}
+//pobiera dane o \hlines i \clines oraz o height
+Importer.manageFirstCell = function(code){
+    const hlines = [];
+    let height;
+    let lineIdx = null;
+
+    for(let i = 0; i<code.length; i++){
+        if(code[i] == "\\"){
+            const cmd = Importer.getCommand(code, i+1);
+            if(cmd[0] == "hline"){
+                hlines.push(-1);
+                if(lineIdx == null) lineIdx = i;
+            } else if(cmd[0] == "cline"){
+                hlines.push(cmd[1]);
+                if(lineIdx == null) lineIdx = i;
+            }
+        }
+        else if(code[i] == '['){
+            if(lineIdx == null){
+                //get height
+                let j = i+1;
+                let hgh = "";
+                while(code[j]!=']'){
+                    hgh += code[j++];
+                }
+                height = hgh;
+                lineIdx = i;
+            }
+        } else if(lineIdx == null){
+            var regex = /^\s*\S+(\s?\S)*\s*$/;
+            if(regex.test(code[i])) lineIdx = i;
+        }
+    }
+    return [hlines, height];
+}
+//usuwa komendy
+Importer.clearFirstCell = function(content, tab){
+    let result = content;
+    if(tab[1]){
+        result = result.replace("[", "");
+        result = result.replace(tab[1], "");
+        result = result.replace("]", "");
+    }
+    if(tab[0]){
+        if(tab[0][0] == -1){
+            result = result.replace("\\hline", "");
+        } else {
+            for(let c = 0; c < tab[0].length; c++){
+                result = result.replace("\\cline{"+tab[0][c]+"}", "");
+            }
+        }
+    }
+    return result.trim();
+}
+//przekszalca dane z cline na indexy kolumn
+Importer.getIdFromCline = function(code){
+    const ids = [];
+    for(let c = 0; c < code.length; c++){
+        const val = code[c].split('-');
+        for(let i = parseInt(val[0].trim()) - 1; i < parseInt(val[1].trim()); i++){
+            ids.push(i);
+        }
+    }
+    return ids;
 }
